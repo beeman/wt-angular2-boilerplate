1f5e7c6be426124f7f97a0650ab7b0d5ce2faad3
Younes JAAIDI <yjaaidi@gmail.com>
2017-02-01 10:03:22 +0100

    Removed tslint configuration from webpack.

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -212,19 +212,7 @@ module.exports = function (options) {
           fileLoader: {
               context: 'src',
               name: '[path][name].[ext]'
-          },
-
-          /**
-           * Static analysis linter for TypeScript advanced options configuration
-           * Description: An extensible linter for the TypeScript language.
-           *
-           * See: https://github.com/wbuchwalter/tslint-loader
-           */
-          tslint: {
-            emitErrors: false,
-            failOnHint: false,
-            resourcePath: 'src'
-          },
+          }
 
         }
       }),
